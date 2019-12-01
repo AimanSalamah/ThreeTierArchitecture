e2c8b66ef42a05e9a5343a0084be7d90f862748a
Aiman Salamah <aiman@root.sa>
2019-12-01 21:53:55 +0300

    Add project files.

---
+++ Shared/Db/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Db
+{
+    public class Student
+    {
+        public Guid ID { get; set; }
+        public string Code { get; set; }
+        public int No { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string UserID { get; set; }
+    }
+}
---
+++ Shared/Vm/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Vm
+{
+    public class Student
+    {
+        public string Code { get; set; }
+        public int No { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
